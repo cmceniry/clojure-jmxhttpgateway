9de3827af5b0afc10875456ceda1336e648ef9bf
Chris McEniry <cmceniry@corgalabs.com>
2010-05-03 22:55:42 -0700

    * first pass on lein setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jmxhttpgateway "0.0.1"
+  :description "A simple http server that gateways jmx calls"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [compojure "0.3.2"]]
+  :namespaces [jmxhttpgateway])
