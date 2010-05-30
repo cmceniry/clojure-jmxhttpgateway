8a2cb7c0d241109eca80fe558dcb8f36c379e467
Chris McEniry <cmceniry@corgalabs.com>
2010-05-29 20:28:40 -0700

    * use leiningen-init-script to get a daemonizing init

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [compojure "0.3.2"]]
+  :dev-dependencies [[leiningen-init-script "0.1.0"]]
+  :lis-opts {:install-dir "/usr/lib/jmxhttpgateway"}
   :namespaces [jmxhttpgateway.server
                jmxhttpgateway.utils]
   :main jmxhttpgateway/server)
